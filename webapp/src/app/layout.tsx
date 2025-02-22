48e8a8c4158a57327bd86cb3f10389d8bc1d0f13
Kshitij Thareja <kshitijthareja03@gmail.com>
2025-02-22 19:42:29 +0530

    Add sign up and login option

--- webapp/src/app/layout.tsx
+++ webapp/src/app/layout.tsx
@@ -1,20 +1,24 @@
+"use client"
 import type { Metadata } from "next";
 import { space_grotesk, akatab } from "./utils/fonts";
+import { Navbar } from "@/components/navbar";
+import { useDarkMode } from '../hooks/useDarkMode';
 import "./globals.css";
-
-export const metadata: Metadata = {
-  title: "mARK.it",
-  description: "Your bookmarks, simplified.",
-};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { darkMode, setDarkMode } = useDarkMode();
   return (
     <html suppressHydrationWarning lang="en" className={`${space_grotesk.variable} ${akatab.variable}`}>
+      <head>
+        <title>mARK.it</title>
+        <meta name="description" content="Your bookmarks, simplified." />
+      </head>
       <body className="font-fspace_grotesk">
+        <Navbar darkMode={darkMode} toggleDarkMode={() => setDarkMode(!darkMode)} />
         {children}
       </body>
     </html>
