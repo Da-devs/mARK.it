3dea524db5792716f7cd35e9c59d71379adc0c0d
Kshitij Thareja <kshitijthareja03@gmail.com>
2025-02-23 10:20:50 +0530

    Fix browser runtime API and make it work with the extension

--- webapp/src/app/utils/authContext.tsx
+++ webapp/src/app/utils/authContext.tsx
@@ -3,6 +3,8 @@ import React, { createContext, useContext, useEffect, useState } from 'react';
 import { createClient } from "@/app/utils/supabase/client";
 import { User } from '@supabase/supabase-js';
 
+// declare const chrome: any;
+
 interface AuthContextType {
   user: User | null;
   refreshUser: () => Promise<void>;
@@ -26,7 +28,20 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     refreshUser();
 
     const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
-      setUser(session?.user ?? null);
+      if (_event === 'SIGNED_OUT') {
+        setUser(null);
+        localStorage.removeItem('userUUID'); // Clear UUID on logout
+      } else if (session?.user) {
+        setUser(session.user);
+        localStorage.setItem('userUUID', session.user.id); // Store UUID on login
+        const browserAPI = typeof chrome !== 'undefined' ? chrome : browser;
+        if (browserAPI.runtime && browserAPI.runtime.sendMessage) {
+          console.log("Sending message to extension with UUID:", session.user.id);
+          browserAPI.runtime.sendMessage("ipagofgebmmdoifcellidcopecibjmom", { uuid: session.user.id }, (response: any) => {
+          console.log("Response from extension:", response);
+          });
+          }
+      }
     });
 
     return () => subscription.unsubscribe();
